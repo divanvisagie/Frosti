795ba06e4c7cb03e945f5e1673f9e4d716d2cfa9
Divan Visagie <visagiedivan@gmail.com>
2024-04-05 22:04:41 +0200

    Rename main file

--- Frosti.ino
+++ Frosti.ino
@@ -2,6 +2,8 @@
 
 // How many internal neopixels do we have? some boards have more than one!
 #define NUMPIXELS        1
+
+// We have to override the defaults for this device
 #define PIN_NEOPIXEL     39
 #define NEOPIXEL_POWER   38
 
@@ -11,28 +13,18 @@ Adafruit_NeoPixel pixels(NUMPIXELS, PIN_NEOPIXEL, NEO_GRB + NEO_KHZ800);
 void setup() {
   Serial.begin(115200);
 
-#if defined(NEOPIXEL_POWER)
-  // If this board has a power control pin, we must set it to output and high
-  // in order to enable the NeoPixels. We put this in an #if defined so it can
-  // be reused for other boards without compilation errors
   pinMode(NEOPIXEL_POWER, OUTPUT);
   digitalWrite(NEOPIXEL_POWER, HIGH);
-#endif
 
   pixels.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
-  pixels.setBrightness(20); // not so bright
+  pixels.setBrightness(10); // not so bright
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
-  // say hi
-  Serial.println("Hello!");
-  Serial.println(NEOPIXEL_POWER);
-  Serial.println(PIN_NEOPIXEL);
-
   
   // set color 
-  pixels.fill(0xFF22FF);
+  pixels.fill(0x990077);
   pixels.show();
   delay(500); // wait half a second
 
